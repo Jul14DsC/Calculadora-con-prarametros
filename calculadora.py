55f70f7ecb7211b1bd514ee992267bba2e0bc16d
juan camilo leiton <leitonnjuann@gmail.com>
2020-08-25 17:21:54 -0500

    se agrego multiplicacion y division 1.2

--- calculadora.py
+++ calculadora.py
@@ -14,5 +14,13 @@ def suma(n1, n2):
 def resta(n1, n2):
     return "El resultado de la resta es: {}" .format(n1 - n2)
 
+@app.route('/multiplicacion/<int:n1>/<int:n2>')
+def multiplicacion(n1, n2):
+    return "El resultado de la suma es: {}" .format(n1 * n2)
+
+@app.route('/division/<int:n1>/<int:n2>')
+def division(n1, n2):
+    return "El resultado de la resta es: {}" .format(n1 / n2)
+
 if __name__ == '__main__':
     app.run(port = 8000, debug = True)
