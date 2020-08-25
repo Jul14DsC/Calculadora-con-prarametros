3b3bd9404b47f7af9df1092428f4a7d6add59a43
Maik Giraldo <michaelgiraldo40@gmail.com>
2020-08-25 16:41:45 -0500

    Se añadio suma y resta a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,18 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Calculadora con parametros"
+
+@app.route('/suma/<int:n1>/<int:n2>')
+def suma(n1, n2):
+    return "El resultado de la suma es: {}" .format(n1 + n2)
+
+@app.route('/resta/<int:n1>/<int:n2>')
+def resta(n1, n2):
+    return "El resultado de la resta es: {}" .format(n1 - n2)
+
+if __name__ == '__main__':
+    app.run(port = 8000, debug = True)
