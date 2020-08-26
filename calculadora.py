6f185a54c980d2afe750d9c15b4c299aa093a309
Julian Sabogal <juliansabogal116@gmail.com>
2020-08-25 20:45:20 -0500

    Operaciones de potencia y raiz cuadrada

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import math
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,6 +21,14 @@ def multiplicacion(n1, n2):
 @app.route('/division/<int:n1>/<int:n2>')
 def division(n1, n2):
     return "El resultado de la resta es: {}" .format(n1 / n2)
+
+@app.route('/potencia/<int:n1>/<int:n2>')
+def potencia(n1,n2):
+    return "El resultado de la potencia es: {}" .format(n1 ** n2)
+
+@app.route('/raiz/<int:n1>')
+def raiz(n1):
+    return "El resultado de la raiz cuadrada es: {}" .format(math.sqrt(n1))
 
 if __name__ == '__main__':
     app.run(port = 8000, debug = True)
